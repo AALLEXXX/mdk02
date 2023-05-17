e1c9f185b084db47eaedc9873c87257610c5599e
Alex <118309685+AALLEXXX@users.noreply.github.com>
2023-05-17 21:17:26 +0300

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1, number2;
+            string formula;
+            double ieeeRemainder, remainder;
+
+            (number1, number2) = (3, 2);
+            formula = $"{number1} / {number2} = ";
+            ieeeRemainder = Math.IEEERemainder(number1, number2);
+            remainder = number1 % number2;
+            Console.WriteLine($"{formula,-16} {ieeeRemainder,18} {remainder,20}");
+
+            (number1, number2) = (4, 2);
+            formula = $"{number1} / {number2} = ";
+            ieeeRemainder = Math.IEEERemainder(number1, number2);
+            remainder = number1 % number2;
+            Console.WriteLine($"{formula,-16} {ieeeRemainder,18} {remainder,20}");
+
+            (number1, number2) = (10, 3);
+            formula = $"{number1} / {number2} = ";
+            ieeeRemainder = Math.IEEERemainder(number1, number2);
+            remainder = number1 % number2;
+            Console.WriteLine($"{formula,-16} {ieeeRemainder,18} {remainder,20}");
+
+            (number1, number2) = (11, 3);
+            formula = $"{number1} / {number2} = ";
+            ieeeRemainder = Math.IEEERemainder(number1, number2);
+            remainder = number1 % number2;
+            Console.WriteLine($"{formula,-16} {ieeeRemainder,18} {remainder,20}");
+
+            (number1, number2) = (27, 4);
+            formula = $"{number1} / {number2} = ";
+            ieeeRemainder = Math.IEEERemainder(number1, number2);
+            remainder = number1 % number2;
+            Console.WriteLine($"{formula,-16} {ieeeRemainder,18} {remainder,20}");
+
+            (number1, number2) = (28, 5);
+            formula = $"{number1} / {number2} = ";
+            ieeeRemainder = Math.IEEERemainder(number1, number2);
+            remainder = number1 % number2;
+            Console.WriteLine($"{formula,-16} {ieeeRemainder,18} {remainder,20}");
+
+            (number1, number2) = (17.8, 4);
+            formula = $"{number1} / {number2} = ";
+            ieeeRemainder = Math.IEEERemainder(number1, number2);
+            remainder = number1 % number2;
+            Console.WriteLine($"{formula,-16} {ieeeRemainder,18} {remainder,20}");
+
+            (number1, number2) = (17.8, 4.1);
+            formula = $"{number1} / {number2} = ";
+            ieeeRemainder = Math.IEEERemainder(number1, number2);
+            remainder = number1 % number2;
+            Console.WriteLine($"{formula,-16} {ieeeRemainder,18} {remainder,20}");
+
+
+        }
+    }
+}
