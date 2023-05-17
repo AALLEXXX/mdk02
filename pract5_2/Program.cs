d39127bbafbade093fd611b43d6d53a2e9254ee3
Alex <118309685+AALLEXXX@users.noreply.github.com>
2023-05-17 22:31:05 +0300

    Add files via upload

---
+++ pract5_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract5_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите предложение:");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+
+            Console.WriteLine("Результат:");
+
+            foreach (string word in words)
+            {
+                if (word.ToLower() != "hello")
+                {
+                    Console.WriteLine(word);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
